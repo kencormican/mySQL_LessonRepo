0b576bf2e50b02a89da624b1a8534805a287e359
kencormican <ken.cormican@bt.com>
2020-10-06 19:25:15 +0000

    Delete multiple rows from list with Delete where in lesson code Part#3

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -13,11 +13,9 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor() as cursor:
-        # Insert multiple rows with "rows" and cursor.executemany() method
-        rows = [("bob", 21, "1990-02-06 23:04:56"),
-                ("jim", 56, "1955-05-09 13:12:45"),
-                ("fred", 100, "1911-09-12 01:01:01")]
-        cursor.executemany("INSERT INTO Friends VALUES (%s,%s,%s);", rows)
+        # Delete multiple rows based on list of names
+        names = ['jim', 'bob']
+        cursor.execute("DELETE FROM Friends WHERE name in (%s, %s)", names)
         connection.commit()
 
 finally:
